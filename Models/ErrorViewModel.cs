531a942ddd8409000a8d3b5e7a83805622164766
Jonathan Carballo <jonathanfrancisco.carballo@unab.edu.sv>
2023-04-22 21:59:32 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebPracticaS12.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
